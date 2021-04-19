02c59ec5989fbc5995021265491e043f72d14f4b
MakeItMike <mbwoodyard@hotmail.com>
2021-04-18 21:55:50 -0400

    Finished!

--- server.js
+++ server.js
@@ -12,15 +12,24 @@ server.listen(port, function () {
     console.log("Server is running on "+ port +" port");
 });
 
-
-// Socket
+// Server
 const users = {};
+
 io.on('connection', function(socket){
     console.log("user connected");
 
+    socket.on('new-user', username => {
+        users[socket.id] = username;
+        socket.broadcast.emit('user-connected', username);
+    });
+
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+    });
+    
     socket.on('disconnect', function(){
+        socket.broadcast.emit('user-disconnected', users[socket.id]);
         console.log('user ' + users[socket.id] + ' disconnected');
-        // remove saved socket from users object
         delete users[socket.id];
   });
 });
